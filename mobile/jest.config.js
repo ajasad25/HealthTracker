81c049aaa303d943f2b5f455dcd1381f6f4f52a9
ajasad25 <asad.meeran25@gmail.com>
2026-05-19 13:22:52 +0500

    fix(mobile): jest-expo preset; setupFilesAfterEnv; 35 tests green

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|expo|@expo|nativewind|react-native-css-interop|expo-secure-store|expo-modules-core|expo-notifications|@react-navigation|react-native-screens|react-native-safe-area-context|react-native-svg|react-native-chart-kit)/)',
-  ],
-  setupFilesAfterSetup: ['./jest.setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   testMatch: ['**/src/__tests__/**/*.test.(ts|tsx)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|nativewind|react-native-css-interop))',
+  ],
 };
